1b6af38b702409b69c5ccec1b61a47f63da6443c
katiebreivik <kbreivik@flatironinstitute.org>
2024-07-13 12:47:41 -0400

    local changes

--- src/scripts/fig1.py
+++ src/scripts/fig1.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 import legwork as lw
 import astropy.units as u
 from scipy.interpolate import interp1d
+from scipy.integrate import trapezoid, cumulative_trapezoid
 from astropy.cosmology import Planck18, z_at_value
 from scipy.integrate import trapezoid
 import paths
 import deepdish as dd
+from matplotlib import colors
 from schwimmbad import MultiPool
 import tqdm
 import utils
+import paths
+
 
 plt.rc('font', family='serif')
 plt.rcParams['text.usetex'] = False
@@ -23,162 +27,20 @@ params = {'figure.figsize': (6,4),
           'ytick.labelsize': 0.7 * fs}
 plt.rcParams.update(params)
 
-def get_LIGO_rate(down_samp_fac=1):
-    # this is lifted ~exactly~ from the GWTC-3 tutorial
-    mass_PP_path = paths.data / "o1o2o3_mass_c_iid_mag_iid_tilt_powerlaw_redshift_mass_data.h5"
-    with open(mass_PP_path, 'r') as _data:
-        _data = dd.io.load(mass_PP_path)
-    
-    #import pdb
-    #pdb.set_trace()
-    dN_dm1dqdVcdt = _data['ppd'].T
-    mass_1 = np.linspace(2, 100, 1000)
-    mass_ratio = np.linspace(0.1, 1, 500)
-    M1, Q = np.meshgrid(mass_1, mass_ratio, indexing='ij')
-    
-    if down_samp_fac > 1:
-        mass_1 = mass_1[::down_samp_fac]
-        mass_ratio = mass_ratio[::down_samp_fac]
-        M1 = M1[::down_samp_fac, ::down_samp_fac]
-        Q = Q[::down_samp_fac, ::down_samp_fac]
-        dN_dm1dqdVcdt = dN_dm1dqdVcdt[::down_samp_fac, ::down_samp_fac]
-
-    return mass_1*u.Msun, mass_ratio, M1*u.Msun, Q, dN_dm1dqdVcdt*u.Msun**(-1) * u.Gpc**(-3) * u.yr**(-1)
-
-def get_horizon_and_chirp(dat_in):
-    m1, q, f = dat_in
-    m2 = m1 * q
-    mc = lw.utils.chirp_mass(m1, m2)
-
-    s = lw.source.Source(m_1=m1 * np.ones(len(f)),
-                         m_2=m2 * np.ones(len(f)),
-                         ecc=np.zeros(len(f)),
-                         f_orb=f,
-                         dist=8 * np.ones(len(f)) * u.Mpc,
-                         interpolate_g=False,
-                         gw_lum_tol=0.001)
-    
-    
-    snr = s.get_snr(t_obs = 8 * u.yr, approximate_R=True, verbose=False)
-    d_h_1 = snr * 8 * u.Mpc
-    d_h_7 = snr / 7 * 8 * u.Mpc    
-    d_h_12 = snr / 12 * 8 * u.Mpc
-    
-    
-    d_h_list = [d_h_1.to(u.Gpc), d_h_7.to(u.Gpc), d_h_12.to(u.Gpc)]
-    V_h_list = []
-    for d_h in d_h_list:
-        V_h = 4/3 * np.pi * d_h**3
-
-        d_mask = d_h > 100 * u.kpc
-        redshift = np.ones(len(d_h)) * 1e-8
-        redshift[d_mask] = z_at_value(Planck18.luminosity_distance, d_h[d_mask])
-        V_h[d_mask] = Planck18.comoving_volume(z=redshift[d_mask])
-        
-        V_h_list.append(V_h.to(u.Gpc**3))
-
-    f_dot = lw.utils.fn_dot(m_c=mc, e=np.zeros(len(f)), n=1, f_orb=f)
-    
-    d_h_list = [d_h.value for d_h in d_h_list]
-    V_h_list = [V_h.value for V_h in V_h_list]
-    
-    return d_h_list, V_h_list, f_dot.to(u.Hz/u.s).value
-
 
 # set up the LISA frequency grid
-f_LISA_grid = np.logspace(-1, -5, 500) * u.Hz
+f_LISA = np.logspace(-1, -5, 150) * u.Hz
+
+# set up the LIGO eccentricity range
+e_LIGO = np.logspace(-6, np.log10(0.0005), 10)
+e_LIGO = np.append(0, e_LIGO)
+e_LIGO_round = np.array([f"{e:.2e}" for e in e_LIGO])
 
 
 # get the mass, mass ratio, and rate grids
-down_samp_fac=15
-mass_1, mass_ratio, M1, Q, dN_dm1dqdVcdt = get_LIGO_rate(down_samp_fac=down_samp_fac)
+down_samp_fac=25
+mass_1, mass_ratio, M1, Q, dN_dm1dqdVcdt = utils.get_LIGO_rate(down_samp_fac=down_samp_fac)
 
-# run on 98 processors
-nproc=128
-
-# loop over the mass and mass ratio grid to get the horizon distance
-dat_in = []
-for m1, q in zip(M1.flatten(), Q.flatten()):
-    dat_in.append([m1, q, f_LISA_grid])
-    
-with MultiPool(processes=nproc) as pool:
-    dat_out = list(tqdm.tqdm(pool.imap(get_horizon_and_chirp, dat_in), total=len(dat_in)))
-
-D_H_1 = []
-D_H_7 = []
-D_H_12 = []
-V_H_1 = []
-V_H_7 = []
-V_H_12 = []
-F_DOT = []
-for d in dat_out:
-    d_h, V_h, f_dot = d
-
-    D_H_1.append(d_h[0])
-    D_H_7.append(d_h[1])
-    D_H_12.append(d_h[2])
-
-    V_H_1.append(V_h[0])
-    V_H_7.append(V_h[1])
-    V_H_12.append(V_h[2])
-    
-    F_DOT.append(f_dot)
-    
-D_H_1 = np.reshape(D_H_1, (len(mass_1), len(mass_ratio), len(f_LISA_grid))) * u.Gpc    
-V_H_1 = np.reshape(V_H_1, (len(mass_1), len(mass_ratio), len(f_LISA_grid))) * u.Gpc**3    
-D_H_7 = np.reshape(D_H_7, (len(mass_1), len(mass_ratio), len(f_LISA_grid))) * u.Gpc    
-V_H_7 = np.reshape(V_H_7, (len(mass_1), len(mass_ratio), len(f_LISA_grid))) * u.Gpc**3    
-D_H_12 = np.reshape(D_H_12, (len(mass_1), len(mass_ratio), len(f_LISA_grid))) * u.Gpc    
-V_H_12 = np.reshape(V_H_12, (len(mass_1), len(mass_ratio), len(f_LISA_grid))) * u.Gpc**3    
-F_DOT = np.reshape(F_DOT, (len(mass_1), len(mass_ratio), len(f_LISA_grid))) * u.Hz / u.s
-
-# get the rates
-dN_dm1_list = []
-N_LISA_obs_list = []
-for v in [V_H_1, V_H_7, V_H_12]:
-    dN_dm1 = np.zeros(len(mass_1)) / u.Msun
-    
-    for ii, m in enumerate(mass_1):
-        dN_dm1dq = np.zeros(len(mass_ratio)) / u.Msun
-        for jj, q in enumerate(mass_ratio):
-            dN_dm1dq[jj] = trapezoid(dN_dm1dqdVcdt[ii,jj] / -F_DOT[ii,jj,:] * v[ii,jj,:], f_LISA_grid).to(1/u.Msun)
-        dN_dm1[ii] = trapezoid(dN_dm1dq, mass_ratio)
-    N_LISA_obs=trapezoid(dN_dm1, mass_1)
-    print(f'The number of LISA sources in total is: {np.round(N_LISA_obs, 2)}')
-
-    dN_dm1_list.append(dN_dm1)    
-    N_LISA_obs_list.append(N_LISA_obs)
-dN_dm1_LIGO = trapezoid(dN_dm1dqdVcdt, mass_ratio, axis=1)                        
-
-
-fig, ax1 = plt.subplots(figsize=(6,4))
-
-# Plot data on the first y-axis
-color = 'black'
-ax1.set_xlabel(r'$M_{BH,1}$ [$M_{\odot}$]', size=16)
-ax1.set_ylabel(r'$dN_{\rm{LISA}}/dM_{BH,1}$ [$M_{\odot}^{-1}$]', color=color, size=16)
-for dN_dm1, ls, snr_r, N_LISA_obs in zip(dN_dm1_list, ['-', '--', '-.'], [1,7,12], N_LISA_obs_list):
-    ax1.plot(mass_1, dN_dm1.value, color=color, label=f'SNR > {snr_r}', ls=ls)
-    
-ax1.tick_params(axis='y', labelcolor=color, labelsize=12)
-ax1.set_yscale('log')
-ax1.set_ylim(1e-5, 3e2)
-# Create a second y-axis that shares the same x-axis
-ax2 = ax1.twinx()
-
-# Plot data on the second y-axis
-color = 'navy'
-ax2.set_ylabel(r'$dN_{\rm{intrinsic}}/dM_{BH,1}$ [$M_{\odot}^{-1}$]', color=color, size=16)
-ax2.plot(mass_1, dN_dm1_LIGO, color=color)
-ax2.tick_params(axis='y', labelcolor=color, labelsize=12)
-ax2.set_yscale('log')
-ax2.set_ylim(5e-5, 7)
-plt.minorticks_on()
-ax1.set_xlim(0, 95)
-ax1.legend(prop={"size":12}, ncol=3, loc=(0, 1.01))
-
-
-#plt.minorticks_on()
-fig.tight_layout()
-fig.savefig(paths.figures / 'fig1_8yr.png', facecolor='white', dpi=100)
-    
+mass_1 = mass_1
+mass_ratio = mass_ratio
+MM, QQ, EE_LIGO, FF = np.meshgrid(mass_1, mass_ratio, e_LIGO, f_LISA, indexing='ij')
--- src/scripts/fig2.py
+++ src/scripts/fig2.py
@@ -11,6 +11,7 @@ from schwimmbad import MultiPool
 import tqdm
 import utils
 import paths
+import cmasher as cmr
 
 plt.rc('font', family='serif')
 plt.rcParams['text.usetex'] = False
@@ -42,61 +43,112 @@ mass_1 = mass_1
 mass_ratio = mass_ratio
 MM, QQ, EE_LIGO, FF = np.meshgrid(mass_1, mass_ratio, e_LIGO, f_LISA, indexing='ij')
 
-dat_in = list(zip(EE_LIGO.flatten(), FF.flatten(), MM.flatten(), QQ.flatten()*MM.flatten()))
-
-with MultiPool(processes=96) as pool:
-    dat_out = list(tqdm.tqdm(pool.imap(utils.get_e_LISA_t_LIGO, dat_in), total=len(dat_in)))
-    
-EE_LISA, TT_LIGO = zip(*dat_out)
-
-EE_LISA = np.array(EE_LISA).reshape(FF.shape)
-TT_LIGO = np.array(TT_LIGO).reshape(FF.shape) * u.yr
-
-np.save(paths.data / 't_merge', TT_LIGO.value)
-np.save(paths.data / 'e_LISA', EE_LISA)
-
-def chunk_list(long_list, num_chunks):
-    avg = len(long_list) / float(num_chunks)
-    chunks = []
-    last = 0.0
-
-    while last < len(long_list):
-        chunks.append(long_list[int(last):int(last + avg)])
-        last += avg
-
-    return chunks
-
-
-num_chunks = 10
-
-snr_thresh = 12
-dat_in = list(zip(MM.flatten(), QQ.flatten(), EE_LISA.flatten(), FF.flatten(), snr_thresh * np.ones(len(MM.flatten()))))
-
-chunked_list = chunk_list(dat_in, num_chunks)
-dat_list = []
-for ii, chunk in enumerate(chunked_list):
-    print('running chunk: ' + str(ii))
-    with MultiPool(processes=96) as pool:
-        dat_out = list(tqdm.tqdm(pool.imap(utils.get_Vc_Dh, chunk), total=len(chunk)))
-        dat_list.extend(dat_out)
-DH, VC = zip(*dat_list)
-DH = np.array(DH).reshape(QQ.shape) * u.Gpc
-VC = np.array(VC).reshape(QQ.shape) * u.Gpc**3
-
-np.save(paths.data / f'comoving_volume_{ii}', VC.value)
-np.save(paths.data / f'horizon_distance_{ii}', DH.value)
-
-#dT_LIGO_df_LISA = utils.dTmerger_df(MM, QQ*MM, FF, EE_LISA).to(u.yr / u.Hz)
-
-#ind_m_10 = 4
-#ind_m_35 = 17
-#ind_m_80 = 40
-#ind_q_05 = 11
-#ind_q_09 = 22
-#ind_circ = 0
-#ind_ecc_mid = 7
-#ind_ecc_high = 15
+#dat_in = list(zip(EE_LIGO.flatten(), FF.flatten(), MM.flatten(), QQ.flatten()*MM.flatten()))
 #
-#plt.plot(f_LISA, DH[ind_m_10])
+#with MultiPool(processes=96) as pool:
+#    dat_out = list(tqdm.tqdm(pool.imap(utils.get_e_LISA_t_LIGO, dat_in), total=len(dat_in)))
+#    
+#EE_LISA, TT_LIGO = zip(*dat_out)
 #
-print('all done friend!')
+#EE_LISA = np.array(EE_LISA).reshape(FF.shape)
+#TT_LIGO = np.array(TT_LIGO).reshape(FF.shape) * u.yr
+#
+#np.save(paths.data / 't_merge', TT_LIGO.value)
+#np.save(paths.data / 'e_LISA', EE_LISA)
+
+#def chunk_list(long_list, num_chunks):
+#    avg = len(long_list) / float(num_chunks)
+#    chunks = []
+#    last = 0.0
+#
+#    while last < len(long_list):
+#        chunks.append(long_list[int(last):int(last + avg)])
+#        last += avg
+#
+#    return chunks
+#
+#
+#num_chunks = 10
+#
+#snr_thresh = 12
+#dat_in = list(zip(MM.flatten(), QQ.flatten(), EE_LISA.flatten(), FF.flatten(), snr_thresh * np.ones(len(MM.flatten()))))
+#
+#chunked_list = chunk_list(dat_in, num_chunks)
+#dat_list = []
+#for ii, chunk in enumerate(chunked_list):
+#    print('running chunk: ' + str(ii))
+#    with MultiPool(processes=96) as pool:
+#        dat_out = list(tqdm.tqdm(pool.imap(utils.get_Vc_Dh, chunk), total=len(chunk)))
+#        dat_list.extend(dat_out)
+#DH, VC = zip(*dat_list)
+#DH = np.array(DH).reshape(QQ.shape) * u.Gpc
+#VC = np.array(VC).reshape(QQ.shape) * u.Gpc**3
+#
+#np.save(paths.data / f'comoving_volume_{ii}', VC.value)
+#np.save(paths.data / f'horizon_distance_{ii}', DH.value)
+EE_LISA = np.load(paths.data / 'e_LISA.npy')
+TT_LISA = np.load(paths.data / 't_merge.npy') * u.Gyr
+VC = np.load(paths.data / 'comoving_volume.npy') * u.Gpc**3
+DH = np.load(paths.data / 'horizon_distance.npy') * u.Gpc 
+
+dT_LIGO_df_LISA = utils.dTmerger_df(MM, QQ*MM, FF, EE_LISA).to(u.yr / u.Hz)
+
+snr_thresh_data = 12
+snr_thresh_list = [1, 7, 12]
+DH = DH * snr_thresh_data / snr_thresh_new
+VC = 4/3 * np.pi * DH**3
+
+cs = cmr.take_cmap_colors('cmr.dusk', len(mass_1), cmap_range=(0.15, 0.9), return_fmt='hex')
+
+ind_m_10 = 3
+ind_m_35 = 14
+ind_m_80 = 32
+ind_q05 = 9
+ind_q09 = 18
+ind_circ = 0
+ind_ecc_mid = 4
+ind_ecc_high = 10
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14,3))
+ax1.plot(f_LISA.value, DH[ind_m_10,ind_q09,ind_circ,:].value, ls='-', color=cs[ind_m_10], zorder=10, label=r'$M_1=10\,M_{\odot}$')
+ax2.plot(f_LISA.value, DH[ind_m_10,ind_q09,ind_ecc_mid,:].value, ls='--', color=cs[ind_m_10], zorder=10, label=r'$e_{\rm{LIGO}}=10^{-5}$')
+ax3.plot(f_LISA.value, DH[ind_m_10,ind_q09,ind_ecc_high,:].value, ls=':', color=cs[ind_m_10], zorder=10, label=r'$e_{\rm{LIGO}}=10^{-3}$')
+#plt.fill_between(f_LISA.value, DH[ind_m_10,ind_q09,ind_circ,:].value, DH[ind_m_10,ind_q09,ind_ecc_high,:].value, color=cs[ind_m_10], alpha=0.5, zorder=10)
+
+ax1.plot(f_LISA.value, DH[ind_m_35,ind_q09,ind_circ,:].value, ls='-', color=cs[ind_m_35], zorder=5, label=r'$M_1=35\,M_{\odot}$')
+ax2.plot(f_LISA.value, DH[ind_m_35,ind_q09,ind_ecc_mid,:].value, ls='--', color=cs[ind_m_35], zorder=5)
+ax3.plot(f_LISA.value, DH[ind_m_35,ind_q09,ind_ecc_high,:].value, ls=':', color=cs[ind_m_35], zorder=5)
+#plt.fill_between(f_LISA.value, DH[ind_m_35,ind_q09,ind_circ,:].value, DH[ind_m_35,ind_q09,ind_ecc_high,:].value, color=cs[ind_m_35], alpha=0.5, zorder=5)
+
+
+ax1.plot(f_LISA.value, DH[ind_m_80,ind_q09,ind_circ,:].value, ls='-', color=cs[ind_m_80], label=r'$M_1=80\,M_{\odot}$')
+ax2.plot(f_LISA.value, DH[ind_m_80,ind_q09,ind_ecc_mid,:].value, ls='--', color=cs[ind_m_80])
+ax3.plot(f_LISA.value, DH[ind_m_80,ind_q09,ind_ecc_high,:].value, ls=':', color=cs[ind_m_80])
+#plt.fill_between(f_LISA.value, DH[ind_m_80,ind_q09,ind_circ,:].value, DH[ind_m_80,ind_q09,ind_ecc_high,:].value, color=cs[ind_m_80], alpha=0.5)
+
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+ax1.set_ylim(1e-5, 6)
+ax1.set_xlim(1e-5, 1e-1)
+ax1.set_xlabel(r'$f_{\rm{LISA}}$ [Hz]')
+
+ax2.set_xscale('log')
+ax2.set_yscale('log')
+ax2.set_ylim(1e-5, 6)
+ax2.set_xlim(1e-5, 1e-1)
+ax2.set_xlabel(r'$f_{\rm{LISA}}$ [Hz]')
+
+ax3.set_xscale('log')
+ax3.set_yscale('log')
+ax3.set_ylim(1e-5, 6)
+ax3.set_xlim(1e-5, 1e-1)
+ax3.set_xlabel(r'$f_{\rm{LISA}}$ [Hz]')
+
+ax1.legend(loc='upper left', frameon=False, prop={'size':11})
+ax2.legend(loc='upper left', frameon=False, prop={'size':11})
+ax3.legend(loc='upper left', frameon=False, prop={'size':11})
+
+ax1.set_ylabel('horizon distance [Gpc]')
+
+fig.tight_layout()
+fig.savefig(paths.figures / 'fig2.png', facecolor='white', dpi=100)
